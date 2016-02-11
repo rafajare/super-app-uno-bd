9e61d5ceb282af4f084ff0596fc5c8d426d4cb4e
rafajare <rafa_8_6_96@hotmail.com>
2016-02-11 17:47:05 +0100

    Update randomicon.php

--- randomicon.php
+++ randomicon.php
@@ -1,8 +1,8 @@
 <?php
 
 $datosconexion = array(
-	"servername" => "", 		// PON AQUI LA IP DE TU SERVIDOR
-	"username" => "", 			// PON AQUI EL USUARIO DE LA BD
+	"servername" => "40.115.19.60", 		// PON AQUI LA IP DE TU SERVIDOR
+	"username" => "root", 			// PON AQUI EL USUARIO DE LA BD
 	"password" => "", 			// PON AQUI LA CONTRASEÑA DE LA BD
 	"dbname" => "log"
 	);
